de2da8d3b29aef7c0c50aa1aff2b7716e31d8e9a
Noah Crouch <ncrouch1@uw.edu>
2023-10-06 19:30:20 -0700

    Co-authored-by: AlexanderHayato <AlexanderHayato@users.noreply.github.com>

--- lab1/DE1_SoC.sv
+++ lab1/DE1_SoC.sv
@@ -8,20 +8,52 @@ module DE1_SoC (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, LEDR, V_GPIO);
 	inout  logic [35:0] V_GPIO;	// expansion header 0 (LabsLand board)
 	logic Enter, Exit;
 	
-	photoSensor psensor (.Clk(~V_GPIO[0]), .Rst(~V_GPIO[3]), .Sensor(V_GPIO[6:5]), .Enter(Enter), .Exit(Exit), .HEX0(HEX0), .HEX1(HEX1));
+	photoSensor psensor (.Clk(~V_GPIO[3]), .Rst(~V_GPIO[0]), .Sensor(V_GPIO[6:5]), .Enter(Enter), .Exit(Exit), .HEX0(HEX0), .HEX1(HEX1)
+	, .HEX2(HEX2), .HEX3(HEX3), .HEX4(HEX4), .HEX5(HEX5));
 	
 	assign LEDR[9] = Enter;
-	assign LEDR[7] = Exit;
+	assign LEDR[8] = Exit;
 	
-	assign LEDR[0] = ~V_GPIO[0];
-	assign LEDR[1] = ~V_GPIO[3];
-	assign LEDR[6:2] = 0;
-	assign LEDR[8] = 0;
-	
-	assign HEX2 = 7'b1111111;
-	assign HEX3 = 7'b1111111;
-	assign HEX4 = 7'b1111111;
-	assign HEX5 = 7'b1111111;
-	
-	
+	assign LEDR[7:0] = 0;
+
 endmodule
+
+module DE1_SoC_tb();
+
+	// define signals
+	logic	clk;
+	logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+	logic [9:0] LEDR;
+	logic [35:0] V_GPIO
+	
+	// define parameters
+	parameter T = 20;
+	
+	// instantiate module
+	DE1_SoC dut (.clk, .HEX0, .HEX1, .HEX2, .HEX3, .HEX4, .HEX5, .LEDR, .V_GPIO);
+	
+	// define simulated clock
+	initial begin
+		clk <= 0;
+		forever	#(T/2)	clk <= ~clk;
+	end  // initial clock
+	
+	initial begin
+		SW[9] <= 1; reset <= 1; 	@(posedge clk);
+		SW[9] <= 0; reset <= 0;	@(posedge clk);
+		for (int i = 0; i < 20; i++) begin
+			SW[1:0] 	<=  2'b10; @(posedge clk);
+			SW[1:0] 	<=  2'b11; @(posedge clk);
+			SW[1:0] 	<=  2'b01; @(posedge clk);
+			SW[1:0]  <=  2'b00; @(posedge clk);
+		end
+		for (int i = 0; i < 20; i++) begin
+			SW[1:0] 	<=  2'b01; @(posedge clk);
+			SW[1:0] 	<=  2'b11; @(posedge clk);
+			SW[1:0] 	<=  2'b10; @(posedge clk);
+			SW[1:0] 	<=  2'b00; @(posedge clk);
+		end
+
+		$stop;
+	end
+endmodule  // DE1_SoC_tb
--- lab1/enter_exit_handler.sv
+++ lab1/enter_exit_handler.sv
@@ -1,9 +1,11 @@
+// This module handles the signals for the hex displays based on the current counterstate
+// reset and clock signals
 module enter_exit_handler(clk, reset, counterstate, HEX0, HEX1);
 	input logic clk, reset;
 	input logic [4:0] counterstate;
-	output logic [6:0] HEX0, HEX1;
-	
-<<<<<<< HEAD
+	output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+
+	// 7-segment display variables defined
 	parameter HEXD0 = 7'b1000000;
 	parameter HEXD1 = 7'b1111001;
 	parameter HEXD2 = 7'b0100100; 
@@ -12,115 +14,66 @@ module enter_exit_handler(clk, reset, counterstate, HEX0, HEX1);
 	parameter HEXD5 = 7'b0010010; 
 	parameter HEXD6 = 7'b0000010; 
 	parameter HEXD7 = 7'b1111000; 
-=======
-	enum { s0, s1, s2, s3, s4, s5, s6 ,s7 ,s8, s9, error } ps, ns;
-	
-	parameter HEXD0 = 7'b0000001;
-	parameter HEXD1 = 7'b1001111;
-	parameter HEXD2 = 7'b0010010;
-	parameter HEXD3 = 7'b0000110;
-	parameter HEXD4 = 7'b1001100;
-	parameter HEXD5 = 7'b0100100;
-	parameter HEXD6 = 7'b0100000;
-	parameter HEXD7 = 7'b0001111;
->>>>>>> af10525df4666732e1a9f035e57ffbcdb4fa08dc
 	parameter HEXD8 = 7'b0000000;
 	parameter HEXD9 = 7'b0010000; 
-	
-	always_comb begin
-		case (ps)
-			s0: begin
-				if 		(enter)		ns = s1;
-				else if 	(exit)		ns = error;
-				else						ns = s0;
-			end
-			
-			s1: begin
-				if 		(enter)		ns = s2;
-				else if 	(exit)		ns = s0;
-				else						ns = s1;
-			end
-			
-			s2: begin
-				if 		(enter)		ns = s3;
-				else if 	(exit)		ns = s1;
-				else						ns = s2;
-			end
-			
-			s3: begin
-				if 		(enter)		ns = s4;
-				else if 	(exit)		ns = s2;
-				else						ns = s3;
-			end
-			
-			s4: begin
-				if 		(enter)		ns = s5;
-				else if 	(exit)		ns = s3;
-				else						ns = s4;
-			end
-			
-			s5: begin
-				if 		(enter)		ns = s6;
-				else if 	(exit)		ns = s4;
-				else						ns = s5;
-			end
-			
-			s6: begin
-				if 		(enter)		ns = s7;
-				else if 	(exit)		ns = s5;
-				else						ns = s6;
-			end
-			
-			s7: begin
-				if 		(enter)		ns = s8;
-				else if 	(exit)		ns = s6;
-				else						ns = s7;
-			end
-			
-			s8: begin
-				if 		(enter)		ns = s9;
-				else if 	(exit)		ns = s7;
-				else						ns = s8;
-			end
-			
-			s9: begin
-				if 		(enter)		ns = error;
-				else if 	(exit)		ns = s8;
-				else						ns = s9;
-			end
-			
-			error: begin
-				if 		(reset)		ns = s0;
-				else						ns = error;
-			end
-			
-			default:						ns = s0;
-		endcase
-	end
-			
+
+	parameter HEXDC = 7'b1000110;
+	parameter HEXDL = 7'b1000111;
+	parameter HEXDE = 7'b0000110;
+	parameter HEXDA = 7'b0001000;
+	parameter HEXDR = 7'b0101111;
+	parameter HEXDU = 7'b1000001;
+	parameter HEXDF = 7'b0001110;
+
+	// FF to change HEXs on clock posedge depending on counterstate value
+	// zero counterstate displays CLEAr0, 16 counterstate displays FULL, all middle states display just car count 		
 	always_ff @(posedge clk) begin
-<<<<<<< HEAD
-		if (reset) begin
-			HEX1 <= HEXD0;
+		if (reset | counterstate == 0) begin 	// HEX[5:1] = "CLEAR0"
 			HEX0 <= HEXD0;
+			HEX1 <= HEXDR;
+			HEX2 <= HEXDA;
+			HEX3 <= HEXDE;
+			HEX4 <= HEXDL;
+			HEX5 <= HEXDC;
 		end
-		else if (counterstate == 0) begin
-			HEX1 <= HEXD0;
-			HEX0 <= HEXD0;
+		
+		else if (counterstate == 16) begin // HEX[5:2] = "FULL"
+			HEX0 <= '1;
+			HEX1 <= '1;
+			HEX2 <= HEXDL;
+			HEX3 <= HEXDL;
+			HEX4 <= HEXDU;
+			HEX5 <= HEXDF;
 		end
 		else if (counterstate == 1) begin
+			HEX5 <= '1;
+			HEX4 <= '1;
+			HEX3 <= '1;
+			HEX2 <= '1;
 			HEX1 <= HEXD0;
 			HEX0 <= HEXD1;
 		end
 		else if (counterstate == 2) begin
+			HEX5 <= '1;
+			HEX4 <= '1;
+			HEX3 <= '1;
+			HEX2 <= '1;
 			HEX1 <= HEXD0;
 			HEX0 <= HEXD2;
 		end
 		else if (counterstate == 3) begin
+			HEX5 <= '1;
+			HEX4 <= '1;
+			HEX3 <= '1;
+			HEX2 <= '1;
 			HEX1 <= HEXD0;
 			HEX0 <= HEXD3;
 		end
 		else if (counterstate == 4) begin
+			HEX5 <= '1;
+			HEX4 <= '1;
+			HEX3 <= '1;
+			HEX2 <= '1;
 			HEX1 <= HEXD0;
 			HEX0 <= HEXD4;
 		end
@@ -129,126 +82,86 @@ module enter_exit_handler(clk, reset, counterstate, HEX0, HEX1);
 			HEX0 <= HEXD5;
 		end
 		else if (counterstate == 6) begin
+			HEX5 <= '1;
+			HEX4 <= '1;
+			HEX3 <= '1;
+			HEX2 <= '1;
 			HEX1 <= HEXD0;
 			HEX0 <= HEXD6;
 		end
 		else if (counterstate == 7) begin
+			HEX5 <= '1;
+			HEX4 <= '1;
+			HEX3 <= '1;
+			HEX2 <= '1;
 			HEX1 <= HEXD0;
 			HEX0 <= HEXD7;
 		end
 		else if (counterstate == 8) begin
+			HEX5 <= '1;
+			HEX4 <= '1;
+			HEX3 <= '1;
+			HEX2 <= '1;
 			HEX1 <= HEXD0;
 			HEX0 <= HEXD8;
 		end
 		else if (counterstate == 9) begin
+			HEX5 <= '1;
+			HEX4 <= '1;
+			HEX3 <= '1;
+			HEX2 <= '1;
 			HEX1 <= HEXD0;
 			HEX0 <= HEXD9;
 		end
 		else if (counterstate == 10) begin
+			HEX5 <= '1;
+			HEX4 <= '1;
+			HEX3 <= '1;
+			HEX2 <= '1;
 			HEX1 <= HEXD1;
 			HEX0 <= HEXD0;
 		end
 		else if (counterstate == 11) begin
+			HEX5 <= '1;
+			HEX4 <= '1;
+			HEX3 <= '1;
+			HEX2 <= '1;
 			HEX1 <= HEXD1;
 			HEX0 <= HEXD1;
 		end
 		else if (counterstate == 12) begin
+			HEX5 <= '1;
+			HEX4 <= '1;
+			HEX3 <= '1;
+			HEX2 <= '1;
 			HEX1 <= HEXD1;
 			HEX0 <= HEXD2;
 		end
 		else if (counterstate == 13) begin
+			HEX5 <= '1;
+			HEX4 <= '1;
+			HEX3 <= '1;
+			HEX2 <= '1;
 			HEX1 <= HEXD1;
 			HEX0 <= HEXD3;
 		end
 		else if (counterstate == 14) begin
+			HEX5 <= '1;
+			HEX4 <= '1;
+			HEX3 <= '1;
+			HEX2 <= '1;
 			HEX1 <= HEXD1;
 			HEX0 <= HEXD4;
 		end
 		else if (counterstate == 15) begin
+			HEX5 <= '1;
+			HEX4 <= '1;
+			HEX3 <= '1;
+			HEX2 <= '1;
 			HEX1 <= HEXD1;
 			HEX0 <= HEXD5;
 		end
-		else if (counterstate == 16) begin
-			HEX1 <= HEXD1;
-			HEX0 <= HEXD6;
-=======
-		if (reset)
-			ps <= s0;
-		else
-			ps <= ns;
-	end
-	
-	always_comb begin
-		if (ps == s0) begin
-			HEX[1] <= HEXD0;
-			HEX[0] <= HEXD0;
-		end
-		else if (ps == s1) begin
-			HEX[1] <= HEXD0;
-			HEX[0] <= HEXD1;
-		end
-		else if (ps == s2) begin
-			HEX[1] <= HEXD0;
-			HEX[0] <= HEXD2;
-		end
-		else if (ps == s3) begin
-			HEX[1] <= HEXD0;
-			HEX[0] <= HEXD3;
-		end
-		else if (ps == s4) begin
-			HEX[1] <= HEXD0;
-			HEX[0] <= HEXD4;
-		end
-		else if (ps == s5) begin
-			HEX[1] <= HEXD0;
-			HEX[0] <= HEXD5;
-		end
-		else if (ps == s6) begin
-			HEX[1] <= HEXD0;
-			HEX[0] <= HEXD6;
-		end
-		else if (ps == s7) begin
-			HEX[1] <= HEXD0;
-			HEX[0] <= HEXD7;
-		end
-		else if (ps == s8) begin
-			HEX[1] <= HEXD0;
-			HEX[0] <= HEXD8;
-		end
-		else if (ps == s9) begin
-			HEX[1] <= HEXD0;
-			HEX[0] <= HEXD9;
-		end
-		else if (ps == s10) begin
-			HEX[1] <= HEXD1;
-			HEX[0] <= HEXD0;
-		end
-		else if (ps == s11) begin
-			HEX[1] <= HEXD1;
-			HEX[0] <= HEXD1;
-		end
-		else if (ps == s12) begin
-			HEX[1] <= HEXD1;
-			HEX[0] <= HEXD2;
-		end
-		else if (ps == s13) begin
-			HEX[1] <= HEXD1;
-			HEX[0] <= HEXD3;
-		end
-		else if (ps == s14) begin
-			HEX[1] <= HEXD1;
-			HEX[0] <= HEXD4;
-		end
-		else if (ps == s15) begin
-			HEX[1] <= HEXD1;
-			HEX[0] <= HEXD5;
-		end
-		else if (ps == s16) begin
-			HEX[1] <= HEXD1;
-			HEX[0] <= HEXD6;
->>>>>>> af10525df4666732e1a9f035e57ffbcdb4fa08dc
-		end
-	end
+	end // always_posedge
 endmodule
 
 module enter_exit_handler_tb();
@@ -274,7 +187,7 @@ module enter_exit_handler_tb();
 		reset <= 1;  						@(posedge clk);
 		counterstate <= 0; 	
 		reset <= 0;  						@(posedge clk);
-		for (int i = 0; i < 15; i++) begin
+		for (int i = 0; i < 16; i++) begin
 			counterstate++;				@(posedge clk);
 		end
 		$stop;
