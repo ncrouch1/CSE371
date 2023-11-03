347a83b28a7d223c93d7b7fc30f9a248e9452c8d
Noah Crouch <ncrouch1@uw.edu>
2023-11-03 01:02:01 -0700

    finish DE1_SoC

--- lab4/DE1_SoC.sv
+++ lab4/DE1_SoC.sv
@@ -1,47 +1,11 @@
 // Module to implement lab 4
 
-module DE1_SoC (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, SW, LEDR);
-	// port declarations
-	input  logic CLOCK_50;  // 50MHz clock
-	output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;  // active low
-	input  logic [3:0] KEY;
-	input  logic [9:0] SW;
-	output logic [9:0] LEDR;
-	
-	// logic for task 1
-	logic reset, start, s, done;
-	logic [7:0] A; // 8-bit input
-	assign A = SW[7:0]; // A is controlled by switches 0-7
-	
-	// Synchronous reset, using two DFF's to handle metastability
-	always_ff @(posedge CLOCK_50) begin
-		reset <= ~KEY[0];
-		s <= ~KEY[3];
-		start <= s;
-	end 
-
-	// bit counter logic
-	logic [3:0] result; 
-	
-	bitcounter task1 (
-		.input_a(A), 
-		.s(s), 
-		.clock(CLOCK_50), 
-		.reset(reset), 
-		.result(result), 
-		.done(done)
-		);
-		
-endmodule 
-// Module to implement lab 4
-
-module DE1_SoC (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, V_GPIO);
+module DE1_SoC (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, SW);
     // port declarations
     input  logic CLOCK_50;  // 50MHz clock
     output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;  // active low
-    inout logic [35:0] V_GPIO;
-    logic [3:0] KEY;
-    logic [9:0] SW;
+    input logic [3:0] KEY;
+    input logic [9:0] SW;
 
     // logic for task 1
     logic reset, start, s, done;
@@ -81,7 +45,7 @@ module DE1_SoC (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, V_GPIO);
         .Loc(Loc),
         .Done(Done),
         .Found(Found),
-        .Enable(SW8)
+        .Enable(SW[8])
     );
 
     assign HEX5 = 7'b1111111;
