fa40436007ff155caecbcfe48a923dbfd800defd
SrDrProfessor <122650128+SrDrProfessor@users.noreply.github.com>
2023-11-03 01:30:59 -0700

    Updated to count up to 8

--- lab4/bitcounter_datapath.sv
+++ lab4/bitcounter_datapath.sv
@@ -39,9 +39,9 @@ module bitcounter_datapath (A, clock, result, input_a, clear, r_shift, incr, loa
 			A <= A;
 			
 		if (clear)
-			result <= 4'b0;
+			result <= 4'h0;
 		else if (incr)
-			result <= result + 4'b1;
+			result <= result + 4'h1;
 		else
 			result <= result;
 	end // datapath logic
