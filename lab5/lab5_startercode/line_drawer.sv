40c3c150ed348fe27548d29f71f124cd5dac3c00
Noah Crouch <ncrouch1@uw.edu>
2023-11-08 18:55:12 -0800

    first step

--- lab5/lab5_startercode/line_drawer.sv
+++ lab5/lab5_startercode/line_drawer.sv
@@ -20,14 +20,31 @@ module line_drawer(clk, reset, x0, y0, x1, y1, x, y, done);
 	input logic [10:0]	x0, y0, x1, y1;
 	output logic done;
 	output logic [10:0]	x, y;
-	
+
 	/* You'll need to create some registers to keep track of things
 	 * such as error and direction.
 	 */
 	logic signed [11:0] error;  // example - feel free to change/delete
-	
+	logic handleError, takestep, leftright, updown;
+	assign handleError = updown ? (error < -0.5) : (error > 0.5);
+	assign x_step = leftright ? (x > x1_post) : (x < x1_post);
+	float m = ((y1 - y0) / x1 - x0);
+
 	always_ff @(posedge clk) begin
 		// YOUR CODE HERE
+		if (reset) begin
+			done <= 0;
+			if (x1 < x0)
+				leftright = 0;
+			else if (x1 > x0)
+				leftright = 1;
+			if (y1 > y0) 
+				updown = 0;
+			else if (y1 < y0)
+				updown = 1;
+			error <= updown ? (y0 + 0.5) : (y0 - 0.5); 
+		end
+
 	end  // always_ff
 	
 endmodule  // line_drawer
